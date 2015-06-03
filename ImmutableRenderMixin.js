ff42ab5717c073800f8e211ee3a7e6c15dc505ca
Jonatan Lundin <jonatan.lundin@gmail.com>
2015-06-03 17:59:20 +0200

    Update when the react-router context changes

--- ImmutableRenderMixin.js
+++ ImmutableRenderMixin.js
@@ -5,6 +5,14 @@ var shallowEqualImmutable = require('./shallowEqualImmutable');
 
 var ImmutableRenderMixin = {
   shouldComponentUpdate: function(nextProps, nextState) {
+    // https://github.com/rackt/react-router/issues/866
+    // so our <Link> components are always re-rendered with a route change
+    if (this.context.router) {
+      const changed = this.pureComponentLastPath !== this.context.router.getCurrentPath();
+      this.pureComponentLastPath = this.context.router.getCurrentPath();
+      if (changed) return true;
+    }
+    
     return !shallowEqualImmutable(this.props, nextProps) ||
            !shallowEqualImmutable(this.state, nextState);
   }
